f480366d7ff70a4d30fc4213855455b435b8ff65
unknown <v5zhu@qq.com>
2017-12-17 21:37:36 +0800

    创建SpringCloud工程

---
+++ src/main/java/com/zuiuu/zuiuuspringcloud/ZuiuuSpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.zuiuu.zuiuuspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuiuuSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuiuuSpringcloudApplication.class, args);
+	}
+}
